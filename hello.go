9743070b1a85245ea7881fce0e339cdc92fd75b9
Alex_Morty <a501883466@qq.com>
2018-12-30 16:51:28 +0800

    hello.go

---
+++ hello.go
@@ -0,0 +1,12 @@
+//Go语言以包作为管理单位
+//每个文件必须先声明包
+//程序必须有一个main包
+//一个文件夹只有一个main（）函数
+package main
+
+import "fmt"
+
+func main() {
+        fmt.Println("hello,world，bike") // 输出 hello,world
+        //Go语言语句没有分号
+}
